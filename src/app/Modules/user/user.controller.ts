beec3e4ac7e9b8d125819e84f252e6e9f3c33b9a
PmpPiyas <princemahmudpiyas@gmail.com>
2025-07-14 23:57:54 +0600

    Add Custom Property

--- src/app/Modules/user/user.controller.ts
+++ src/app/Modules/user/user.controller.ts
@@ -36,11 +36,7 @@ const getAllUsers = catchAsync(
 const updateUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.params.id;
-    // const token = req.headers.authorization;
-    // const verifiedToken = verifyToken(
-    //   token as string,
-    //   envVars.JWT_ACCESS_TOKEN
-    // );
+
     const verifiedToken = req.user;
     const user = await UserServices.updateUser(
       userId,
